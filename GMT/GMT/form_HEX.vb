d78361303175858ffced421d27d02af8214e6643
某M <mmnstar@hotmail.com>
2016-12-21 21:43:58 +0800

    161221

--- GMT/GMT/form_HEX.vb
+++ GMT/GMT/form_HEX.vb
@@ -1,5 +1,6 @@
 ﻿Public Class form_HEX
     Private Sub form_HEX_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         'Label2.Text = "版本：" & My.Application.Info.Version.ToString & " 这里用于展示一些DEBUG信息"
     End Sub
 End Class
